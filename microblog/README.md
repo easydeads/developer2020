76bb0253bbd153757e6f72e05024cb9e4406c3b5
easydeads <cacki_pecki@bk.ru>
2020-07-29 16:54:52 +0300

    готово все по 2й статье.

--- microblog/README.md
+++ microblog/README.md
@@ -1,3 +1,4 @@
 1. Перейти в microblog дирректорию
 2. Создать виртуальное окружение python -m venv venv
-3. Установить requirements pip install flask
+3. Установить pip install -r requirements
+4. Для запуска сервера необходимо добавить в переменные окружения находясь в дирректории с файлом app.py - export FLASK_APP=app
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,7 +1,37 @@
 """ URL`s """
+# -*- coding: utf-8 -*-
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return "Hello, World!"
+  user = {'username': 'Miguel'}
+  posts = [
+    {
+      'author': {'username': 'John'},
+      'body': 'Beautiful day in Portaland!'
+    },
+    {
+      'author': {'username': 'Susan'},
+      'body': 'The Avengers movie was so cool!'
+    },
+    {
+      'author': {'username': 'Ипполит'},
+      'body': 'Какая гадость эта ваша заливная рыба!'
+    },
+
+    {
+      'author': {'username': 'John'},
+      'body': 'Beautiful day in Portaland!'
+    },
+    {
+      'author': {'username': 'Susan'},
+      'body': 'The Avengers movie was so cool!'
+    },
+    {
+      'author': {'username': 'Ипполит'},
+      'body': 'Какая гадость эта ваша заливная рыба!'
+    }
+  ]
+  return render_template('index.html', title='Home', user=user, posts=posts)
