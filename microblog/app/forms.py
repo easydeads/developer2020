6f348aef5b3d6f04b4b83697dbeafd28d09585ca
easydeads <cacki_pecki@bk.ru>
2020-07-30 15:38:19 +0300

    добавлена форма входа, все работает

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -5,4 +5,5 @@ from wtforms.validators import DataRequired
 class LoginForm(FlaskForm):
   username = StringField('Username', validators=[DataRequired()])
   password = PasswordField('Password', validators=[DataRequired()])
-  remember = SubmitField('Sign In')
+  remember_me = BooleanField('Remember Me')
+  submit = SubmitField('Sign In')
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -35,3 +36,8 @@ def index():
     }
   ]
   return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+  form = LoginForm()
+  return render_template('login.html', title='Sign In', form=form)
